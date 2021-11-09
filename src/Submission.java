a26c046436fbe3c7ba2e02a61b5840d9ed8545be
liziyu001 <43000743+liziyu001@users.noreply.github.com>
2021-11-09 11:33:18 -0500

    Create Submission.java

---
+++ src/Submission.java
@@ -0,0 +1,18 @@
+public class Submission {
+    private Quiz quiz;
+    private Student student;
+    private boolean graded;
+    int[] answers;
+    int[] subGrades;
+    int totalGrades;
+
+    public Submission(Student student, int[] answers, Quiz quiz) {
+        this.student = student;
+        this.answers = answers;
+        this.quiz = quiz;
+        graded = false;
+        subGrades = new int[answers.length];
+        totalGrades = 0;
+        
+    }
+}
