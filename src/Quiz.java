ab4ddd955b2fdc0813c9aed056864f35e8ae1ef4
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-11-13 18:09:42 -0500

    Update Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Quiz {
     private String name;
     private ArrayList<Question> questions;
+    private ArrayList<Submission> sub;
 
     public Quiz(String name, ArrayList<Question> questions) {
         this.name = name;
         this.questions = questions;
+        this.sub = null;
+    }
+     public Quiz(String name, ArrayList<Question> questions, ArrayList<Submission> sub) {
+        this.name = name;
+        this.questions = questions;
+        this.sub = sub;
     }
     public Quiz(String name){
         this.name = name;
@@ -32,6 +39,12 @@ public class Quiz {
 
     public void setQuestions(ArrayList<Question> questions) {
         this.questions = questions;
+    }
+    public ArrayList<Submission> getSubmission() {
+        return sub;
+    }
+    public void setSubmission(ArrayList<Submission> sub) {
+        this.sub = sub;
     }
     //possiblyuu for grading
     public void Feedback(){
