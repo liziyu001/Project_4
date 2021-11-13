c69ad5cb551a860d98f1897a8fd4d48c8c865f08
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-11-12 23:40:03 -0500

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Teacher extends Account {
     public Teacher(String username, String password) {
@@ -14,16 +14,31 @@ public class Teacher extends Account {
         
     }
 
-    //Randomizes question and option order
+    //Adds the quiz from the file with a randomized question and answer choice order
     public void randomizeQuiz(Course course, String filename) {
         try {
-            Scanner s = new Scanner(new FileReader(filename));
-            while (s.hasNext()) {
-                synchronized (Object.class) {
-                    course.AddQuizFromFile(filename);   
-                }
+            Path filePath = new File(filename).toPath();
+            List<String> stringList = Files.readAllLines(filePath);
+            String[] stringArray = stringList.toArray(new String[]{});
+            String nameOfQuiz = stringArray[0];
+            ArrayList<Question> questions = new ArrayList<>();
+            ArrayList<Quiz> courseQuiz = new ArrayList<>();
+            for (int i = 1; i < stringArray.length; i += 4){
+                String promptOfQuestion = stringArray[i];
+                String answers = stringArray[i+1];
+                int correctAnswer = Integer.parseInt(stringArray[i+2]);
+                String[] str = answers.split(",");
+                List<String> answerChoices = new ArrayList<>();
+                answerChoices = Arrays.asList(str);
+                Question question = new Question(promptOfQuestion, (ArrayList<String>) answerChoices, correctAnswer);
+                questions.add(question);
             }
-          
+            //Shuffles the question and answer choice order 
+            Collections.shuffle(questions);
+            Quiz quiz = new Quiz(nameOfQuiz, questions);
+            courseQuiz.add(quiz);
+            System.out.println("Quiz has been successfully added");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
