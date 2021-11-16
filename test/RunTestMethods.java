def8ec2adfef118839e7317716354ab177e0a9b8
Leo Pan <kinglionleo.8@gmail.com>
2021-11-16 11:55:43 -0500

    TestCases

--- test/RunTestMethods.java
+++ test/RunTestMethods.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,6 +20,16 @@ public class RunTestMethods {
     private ByteArrayOutputStream testOut;
     private final PrintStream originalOutput = System.out;
     private final InputStream originalSysin = System.in;
+
+    private String accessibleQuizzesData = "";
+    private String accountsData = "";
+    private String coursesData = "";
+    private String CS180Data = "";
+    private String CS180QuizData = "";
+    private String CS193Data = "";
+    private String deletedAccountsData = "";
+
+    private String readFile = "";
 
     @Test(timeout = 1000)
     /**
@@ -180,6 +193,63 @@ public class RunTestMethods {
 
     }
 
+    @Test(timeout = 1000)
+    /**
+     * Makes sure that the program can create an account properly
+     */
+    public void testExpectedFive() {
+        // Set the input
+        // Separate each input with a newline (\n).
+        String input = "1\nTestAccount\nTestPassword\n1\n0";
+
+        // Pair the input with the expected result
+        String expected = "Welcome to the System\n" +
+                "1. Create account\n" +
+                "2. Login\n" +
+                "Enter your User ID:\n" +
+                "Enter your Password:\n" +
+                "Your role: 1. Teacher(manage quizzes/courses, grade student's submissions\n" +
+                "2. Student(view courses/quizzes and submit their responses to quizzes)\n" +
+                "1. View Courses\n" +
+                "2. Create a Course\n" +
+                "3. Account Setting\n" +
+                "0. Exit\n" +
+                "Thanks for using our program!";
+
+        // Runs the program with the input values
+        // Replace TestProgram with the name of the class with the main method
+        receiveInput(input);
+        ManagementSystem.main(new String[0]);
+
+        // Retrieves the output from the program
+        String stuOut = getOutput();
+
+        // Trims the output and verifies it is correct.
+        stuOut = stuOut.replace("\r\n", "\n");
+        assertEquals("The output was not as expected.",
+                expected.trim(), stuOut.trim());
+
+        try {
+            readFile = Files.readString(Path.of("accounts.txt"));
+
+            String expectedFile = "1, Leo, hi, true\n" +
+                    "2, Manas, hi, false\n" +
+                    "3, William, test, false\n" +
+                    "4, TestAccount, TestPassword, false\n";
+
+
+            assertEquals("The files were not equal", expectedFile, readFile);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 
 
@@ -271,6 +341,85 @@ public class RunTestMethods {
     } */
 
     @Before
+    public void saveFiles(){
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("accessible_quizzes.txt"));
+            String line = br.readLine();
+            while(line!=null){
+                accessibleQuizzesData += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("accounts.txt"));
+            line = br.readLine();
+            while(line!=null){
+                accountsData += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("courses.txt"));
+            line = br.readLine();
+            while(line!=null){
+                coursesData += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("CS180.txt"));
+            line = br.readLine();
+            while(line!=null){
+                CS180Data += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("CS180_TestQuiz_2021.11.15.17.30.33.txt"));
+            line = br.readLine();
+            while(line!=null){
+                CS180QuizData += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("CS193.txt"));
+            line = br.readLine();
+            while(line!=null){
+                CS193Data += line + "\n";
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("deleted_accounts.txt"));
+            line = br.readLine();
+            while(line!=null){
+                deletedAccountsData += line + "\n";
+                line = br.readLine();
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @After
+    public void revertFiles(){
+        try {
+
+            Files.write(Paths.get("accessible_quizzes.txt"), accessibleQuizzesData.getBytes());
+            Files.write(Paths.get("accounts.txt"), accountsData.getBytes());
+            Files.write(Paths.get("courses.txt"), coursesData.getBytes());
+            Files.write(Paths.get("CS180.txt"), CS180Data.getBytes());
+            Files.write(Paths.get("CS180_TestQuiz_2021.11.15.17.30.33.txt.txt"), CS180QuizData.getBytes());
+            Files.write(Paths.get("CS193.txt"), CS193Data.getBytes());
+            Files.write(Paths.get("deleted_accounts.txt"), deletedAccountsData.getBytes());
+
+        } catch (IOException e) {
+            System.out.println("Error occurred when restoring files");
+        }
+    }
+
+    @Before
     public void outputStart() {
         testOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(testOut));
@@ -280,6 +429,8 @@ public class RunTestMethods {
     public void restoreInputAndOutput() {
         System.setIn(originalSysin);
         System.setOut(originalOutput);
+
+        System.out.println(readFile);
     }
 
     private String getOutput() {
